4d6c831003449d78b2d360a69e26447c7a5e91c3
hamster197 <hamster197@mail.ru>
2020-09-03 12:34:58 +0300

    forst commit

---
+++ apps/shipping/methods.py
@@ -0,0 +1,16 @@
+from decimal import Decimal as D
+from django.utils.translation import ugettext_lazy as _
+from oscar.apps.shipping import methods
+
+
+class SelfPickup(methods.Free):
+    code = 'self-pickup'
+    name = _('Self pickup')
+
+
+class Courier(methods.FixedPrice):
+    code = 'courier'
+    name = _('Courier shipping')
+
+    charge_excl_tax = D('350.00')
+    charge_incl_tax = D('350.00')
---
+++ apps/shipping/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+from oscar.apps.shipping.abstract_models import AbstractOrderAndItemCharges
+
+
+class StandardMethod(AbstractOrderAndItemCharges):
+    """Модель с обычным набором атрибутов"""
+
+    class Meta(AbstractOrderAndItemCharges.Meta):
+        verbose_name = _('Shipping method')
+        verbose_name_plural = _('Shipping methods')
+from oscar.apps.shipping.models import *  # noqa isort:skip
